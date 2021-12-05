fddf1fd6bbf6ce69d601433445e0aa538c5e7e9e
Alexander Vasiliev <asvasi25@gmail.com>
2021-12-05 12:44:13 +0300

    Add global styles and Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './components/Layout/Header';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default App;
