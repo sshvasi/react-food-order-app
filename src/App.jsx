baa0e9468f488c22174a1ca98e2672ec39cb8585
Alexander Vasiliev <asvasi25@gmail.com>
2021-12-06 13:40:12 +0300

    Add Meals component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 };
